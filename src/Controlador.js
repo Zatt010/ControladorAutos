c9ea0c53eef0b1ba6072263c4638c26691c0ff1c
Zatt010 <afuheho1@gmail.com>
2023-08-31 22:37:09 -0400

    Validación de formato para posiciones iniciales

--- src/Controlador.js
+++ src/Controlador.js
@@ -1,6 +1,14 @@
 function controlador(comandos){
-   const Posicion_inicial=comandos
-   return Posicion_inicial
+     // Define una expresión regular para el formato
+   const formatoRegex = /^(\d+),(\d+)([NSEO])$/;
+  // Intenta hacer coincidir la cadena de entrada con la expresión
+   const coincidencia = comandos.match(formatoRegex);
+   if (coincidencia) {
+         return true;
+   }
+   // La cadena de entrada no tiene el formato correcto
+   console.log("Formato incorrecto");
+   return false;
 }
 
 
