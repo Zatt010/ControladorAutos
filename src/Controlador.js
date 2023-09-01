02f7a27596388ea4ea7f77be3b958804d172a87d
Zatt010 <afuheho1@gmail.com>
2023-09-01 11:05:15 -0400

    Refactorización validacion de posicion inicial

--- src/Controlador.js
+++ src/Controlador.js
@@ -1,14 +1,13 @@
 function controlador(comandos){
      // Define una expresión regular para el formato
-   const formatoRegex = /^(\d+),(\d+)([NSEO])$/;
+      const formatoRegex = /^(\d+),(\d+)([NSEO])$/;
   // Intenta hacer coincidir la cadena de entrada con la expresión
-   const coincidencia = comandos.match(formatoRegex);
-   if (coincidencia) {
-         return true;
-   }
+      const coincidencia = comandos.match(formatoRegex);
+      if (coincidencia) {
+            return comandos;
+      }
    // La cadena de entrada no tiene el formato correcto
-   console.log("Formato incorrecto");
-   return false;
+      return false;
 }
 
 
--- src/presenter.js
+++ src/presenter.js
@@ -2,10 +2,15 @@ import controlador from "./Controlador";
 
 const comandos = document.querySelector("#Comandos");
 const form = document.querySelector("#Controlador-form");
-const salida = document.querySelector("#salida-div");
+const posicion_inicial = document.querySelector("#posicion_inicial");
+const mensajes = document.querySelector("#mensajes");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  salida.innerHTML = "<p> Posicion Inicial:" + controlador(comandos.value) + "</p>";
-
+  if (!controlador(comandos.value)) {
+    posicion_inicial.innerHTML = "<p> Posicion Inicial: 0 </p>" ;
+    mensajes.innerHTML =  "<p> Mensajes: Error en el formato  </p>";
+  } else {
+    posicion_inicial.innerHTML = "<p> Posicion Inicial:" + controlador(comandos.value) + "</p>";
+  }
 });
