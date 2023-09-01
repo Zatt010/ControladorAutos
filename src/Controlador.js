f347c3fac29a15cbfd45b083b0d9de979f2de663
Zatt010 <afuheho1@gmail.com>
2023-09-01 16:12:40 -0400

    Refactorización, modulo aparte solo para revision del formato

--- src/Controlador.js
+++ src/Controlador.js
@@ -1,17 +1,8 @@
+import revision from "./RevisionFormato";
+
 function controlador(comandos){
-     // Define una expresión regular para el formato
-      const formatoRegex = /^(\d+),(\d+)\/(\d+),(\d+)([NSEO])$/;
-     // Intenta hacer coincidir la cadena de entrada con la expresión
-      const coincidencia = comandos.match(formatoRegex);
-      if (coincidencia) {
-            const posX = coincidencia[3];
-            const posY = coincidencia[4];
-            const direccion = coincidencia[5];
-            const cadenaDevolucion = `${posX},${posY}${direccion}`;
-            return cadenaDevolucion;
-      }
-   // La cadena de entrada no tiene el formato correcto
-      return false;
+      const respuesta= revision(comandos)
+      return respuesta;
 }
 
 
