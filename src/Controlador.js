cc5fc78aac18997ea1f2ee9dfcb390701e57bcf7
Zatt010 <afuheho1@gmail.com>
2023-09-01 12:11:26 -0400

    Validación de formato al ingresar matriz junto a posicion inicial

--- src/Controlador.js
+++ src/Controlador.js
@@ -1,10 +1,14 @@
 function controlador(comandos){
      // Define una expresión regular para el formato
-      const formatoRegex = /^(\d+),(\d+)([NSEO])$/;
-  // Intenta hacer coincidir la cadena de entrada con la expresión
+      const formatoRegex = /^(\d+),(\d+)\/(\d+),(\d+)([NSEO])$/;
+     // Intenta hacer coincidir la cadena de entrada con la expresión
       const coincidencia = comandos.match(formatoRegex);
       if (coincidencia) {
-            return comandos;
+            const posX = coincidencia[3];
+            const posY = coincidencia[4];
+            const direccion = coincidencia[5];
+            const cadenaDevolucion = `${posX},${posY}${direccion}`;
+            return cadenaDevolucion;
       }
    // La cadena de entrada no tiene el formato correcto
       return false;
