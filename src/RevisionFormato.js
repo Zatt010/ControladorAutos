a738a6011d2d42b1c7f89f851624c5df7a510692
Zatt010 <afuheho1@gmail.com>
2023-09-02 21:36:14 -0400

    Refactorización, Mostrar posicion inicial y comandos sin apretar el boton ejecutar

--- src/RevisionFormato.js
+++ src/RevisionFormato.js
@@ -1,21 +1,21 @@
 function revision(comandos){
-    // Define una expresión regular para el formato el + verifica que por lo menos exista un comando
-    const formatoRegex = /^(\d+),(\d+)\/(\d+),(\d+)([NSEO])\/([IDAV]+)$/;
-    // Intenta hacer coincidir la cadena de entrada con la expresión
-    const coincidencia = comandos.match(formatoRegex);
-    if (coincidencia) {
-        const posX = coincidencia[1];
-        const posY = coincidencia[2];
-        const posXI = coincidencia[3];
-        const posYI = coincidencia[4];
-        if(posXI<=posX &&  posYI<=posY){
-        const direccion = coincidencia[5];
-        const cadenaDevolucion = `${posXI},${posYI}${direccion}`;
-        return cadenaDevolucion;
-        }
-}
-
-// La cadena de entrada no tiene el formato correcto
+  // Define una expresión regular para el formato el + verifica que por lo menos exista un comando
+  const formatoRegex = /^(\d+),(\d+)\/(\d+),(\d+)([NSEO])\/([IDAV]+)$/;
+  // Intenta hacer coincidir la cadena de entrada con la expresión
+  const coincidencia = comandos.match(formatoRegex);
+  if (coincidencia) {
+      const posX = coincidencia[1];
+      const posY = coincidencia[2];
+      const posXI = coincidencia[3];
+      const posYI = coincidencia[4];
+      if (posXI <= posX && posYI <= posY) {
+          const cordinalidad = coincidencia[5];
+          const comandos_movimientos = coincidencia[6];
+          const posicion_inicial = `${posXI},${posYI}${cordinalidad}`;
+          return { posicion_inicial, comandos_movimientos };
+      }
+  }
+  // La cadena de entrada no tiene el formato correcto
   return false;
 }
 
--- src/presenter.js
+++ src/presenter.js
@@ -1,16 +1,20 @@
-import controlador from "./Controlador";
-
-const comandos = document.querySelector("#Comandos");
+import revision from "./RevisionFormato"
+const cadena = document.querySelector("#Cadena");
 const form = document.querySelector("#Controlador-form");
 const posicion_inicial = document.querySelector("#posicion_inicial");
+const comandos = document.querySelector("#Comandos");
 const mensajes = document.querySelector("#mensajes");
-
-form.addEventListener("submit", (event) => {
+form.addEventListener("input", (event) => {
   event.preventDefault();
-  if (!controlador(comandos.value)) {
-    posicion_inicial.innerHTML = "<p> Posicion Inicial: 0 </p>" ;
-    mensajes.innerHTML =  "<p> Mensajes: Error en el formato  </p>";
-  } else {
-    posicion_inicial.innerHTML = "<p> Posicion Inicial:" + controlador(comandos.value) + "</p>";
+  const cadenaValor = cadena.value || ""; // Si cadena.value es undefined usa una vacia
+  const resultadoRevision = revision(cadenaValor);
+  if (resultadoRevision) {// Si la cadena es válida, muestra la posición inicial y los comandos
+    posicion_inicial.innerHTML = `<p>Posición inicial: ${resultadoRevision.posicion_inicial}</p>`;
+    comandos.innerHTML = `<p>Comandos: ${resultadoRevision.comandos_movimientos}</p>`;
+    mensajes.innerHTML = "<p> Mensajes:  </p>";
+  } else {// Si la cadena no es válida
+    posicion_inicial.innerHTML = "<p> Posición Inicial:</p>";
+    comandos.innerHTML ="<p> Comandos:</p>";
+    mensajes.innerHTML = "<p> Mensajes: Error en el formato  </p>";
   }
 });
